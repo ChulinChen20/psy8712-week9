5aa4ff1e712080ae40a1213342a22f028240c77f
[chulinchen20] <[chen6496@umn.edu]>
2024-03-25 06:03:35 -0500

    finish the scraping part

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -30,77 +30,14 @@ vote_comment_corr <- cor.test(
 
 vote_comment_corr
 
-# combine the rounded coefficient and p-value and the degrees of freedom
-corr <- c(round(correlation,2),cor.test(rstats_tbl$upvotes,rstats_tbl$comments)$parameter)
-
-# remove leading zero
-corr <-  sub("^0+", "", corr) 
+# Publication
+"The correlation between upvotes and comments was r(23) = .20, p = .33. This test was not statistically significant."
 
 # paste strings and the dynamic values together
 paste("The correlation between upvotes and comments was r(", vote_comment_corr$parameter,") = ",
       str_remove(format(round(vote_comment_corr$estimate,2),nsmall = 2),"^0"),
       ", p = ",str_remove(format(round(vote_comment_corr$p.value,2),nsmall = 2),"^0"),
-      ". This test ", ifelse(vote_comment_corr$p.value <= 0.05, "was","was not"),
+      ". This test ", ifelse(vote_comment_corr$p.value < 0.05, "was","was not"),
       " statistically significant.", sep="")
-
-# Publication
-"The correlation between upvotes and comments was r(23) = .20, p = .33. This test was not statistically significant."
-
-# Data Import and Cleaning
-# download html data from the link
-rstats_html <- read_html("https://old.reddit.com/r/rstats/")
-
-# scrap titles
-post <- rstats_html %>% 
-  html_elements(css='.self .title.may-blank , .title.outbound , .self .title.may-blank') %>%
-  html_text()
-
-# scrap upvotes
-upvotes <- rstats_html %>% 
-  html_elements(css='.even .score , .odd .score') %>%
-  html_text() %>%
-  as.numeric() 
-
-# the real number of upvotes seems to by every three elements start from position 2
-upvotes<- upvotes[seq(2,length(upvotes),3)]
-
-# scrap number of comments, extract number, replace na with o
-comments <- rstats_html %>% 
-  html_elements(css='.comments') %>%
-  html_text() %>%
-  str_remove_all("\\D") %>%
-  as.numeric() %>%
-  replace_na(0)
-
-# combine the three variables into a tibble
-rstats_tbl <-tibble(post,upvotes,comments)
-
-# Visualization  
-# use scatterplot to visualize the relationship
-rstats_tbl %>%
-  ggplot(aes(upvotes,comments)) +
-  geom_point()
-
-# Analysis
-# create two new variables from result of cor.test
-correlation <- rstats_tbl %>%
-  summarize(coef = cor.test(upvotes,comments)$estimate,
-            p_value = cor.test(upvotes,comments)$p.value)
-
-# Display correlation
-correlation
-
-# combine the rounded coefficient and p-value and the degrees of freedom
-corr <- c(round(correlation,2),cor.test(rstats_tbl$upvotes,rstats_tbl$comments)$parameter)
-
-# remove leading zero
-corr <-  sub("^0+", "", corr) 
-
-# paste strings and the dynamic values together
-paste("The correlation between upvotes and comments was r(", corr[3],") = ",corr[1],", p = ",corr[2],". This test was not statistically significant.", sep="")
-
-# Publication
-"The correlation between upvotes and comments was r(23) = c(cor = 0.16), p = .45. This test was not statistically significant."
-
 
 
---
+++ R/reddit_scraped.R
@@ -0,0 +1,59 @@
+# Script Settings and Resources
+library(tidyverse)
+library(rvest)
+
+# Data Import and Cleaning
+# download html data from the link
+cnbc_html <- read_html("https://www.cnbc.com")
+
+# define a function to scrape headlines and calculate length for one section
+get_headlines <- function(section_url, source) {
+  section_html <- read_html(section_url)
+  headlines <- section_html %>% 
+    html_elements(css='.TrendingNowItem-title , .Card-title') %>%
+    html_text()
+  length = str_count(headlines, "\\S+")
+  
+  # combine the three variables into a tibble
+  cnbc_tbl <-tibble(headlines,length,source)
+  
+  return(cnbc_tbl)
+}
+
+# Define sections
+sections <- c("business", "investing", "tech", "politics")
+
+# create an empty tibble
+cnbc_tbl <-NULL
+
+for (section in sections) {
+  # get section urls
+  section_url <- paste0("https://www.cnbc.com","/",section)
+  # scrape headlines from the given section
+  section_tbl <- get_headlines(section_url,section)
+  cnbc_tbl <-bind_rows(cnbc_tbl,section_tbl)
+}
+
+# Visualization  
+# use scatterplot to visualize the relationship
+cnbc_tbl %>%
+  ggplot(aes(x=source,y=length)) +
+  geom_point()
+
+# Run ANOVA
+anova_result <- aov(length ~ source, data = cnbc_tbl)
+
+anova_summary <- summary(anova_result)
+
+# Publication
+"The results of an ANOVA comparing lengths across sources was F(3, 150) = 2.26, p = .08. This test was not statistically significant."
+
+# paste strings and the dynamic values together
+paste("The results of an ANOVA comparing lengths across sources was F(", 
+      summary(anova_result)[[1]]$"Df"[1], ", ", 
+      summary(anova_result)[[1]]$"Df"[2], ") = ", 
+      str_remove(format(round(anova_summary[[1]]$`F value`[1], 2),nsmall = 2),"^0"),
+      ", p = ", str_remove(format(round(anova_summary[[1]]$"Pr(>F)"[1], 2),nsmall = 2),"^0"), 
+      ". This test ", ifelse(anova_summary[[1]]$"Pr(>F)"[1] < 0.05, "was","was not"),
+      " statistically significant.", sep = "")
+
